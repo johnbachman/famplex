02327d69e32f720f120a1db5eb0a501508870605
Ben Gyori <ben.gyori@gmail.com>
2018-03-29 11:38:39 -0400

    Handle unicode characters in CSV in Python 2

--- check_references.py
+++ check_references.py
@@ -8,9 +8,10 @@ def read_csv(fh, delimiter, quotechar):
     if sys.version_info.major < 3:
         csvreader = csv.reader(fh, delimiter=bytes(delimiter),
                                quotechar=bytes(quotechar))
+        rows = [[cell.decode('utf-8') for cell in row] for row in csvreader]
     else:
         csvreader = csv.reader(fh, delimiter=delimiter, quotechar=quotechar)
-    rows = [row for row in csvreader]
+        rows = [row for row in csvreader]
     return rows
 
 
