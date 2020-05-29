41c49c2f05f380a83d69f7d481e05655e0480a51
steppi <albert_steppi@hms.harvard.edu>
2020-05-28 22:20:01 -0400

    Add function to construct gmap from list of rows

--- famplex/util.py
+++ famplex/util.py
@@ -31,3 +31,29 @@ def load_csv(filename):
                                quotechar=str(u'"'))
         rows = [row for row in csvreader]
     return rows
+
+
+def construct_grounding_map(rows):
+    """Construct grounding map from rows in a grounding_map csv file
+
+    Parameters
+    ----------
+    rows : list
+        List of rows from a grounding map csv file. File should contain seven
+        columns, the first of which is a Famplex ID. The remaining columns
+        contain namespace, id pairs, each pair occupying two columns. Some
+        columns may be blank but file must be padded out with commas.
+
+    Returns
+    -------
+    gmap : dict
+        Dictionary mapping Famplex IDs to INDRA style db_refs dicts. Each
+        db_refs dict maps namespaces to ids.
+    """
+    gmap = {}
+    for row in rows:
+        text = row[0]
+        db_refs = {'TEXT': text}
+        db_refs.update({ns: id_ for ns, id_ in zip(row[1::2], row[2::2])})
+        gmap[text] = db_refs if len(db_refs) > 1 else None
+    return gmap
