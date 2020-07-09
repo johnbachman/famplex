9fed04d889e6f0f40c127f49a733a6cbcb9cbfbf
steppi <albert_steppi@hms.harvard.edu>
2020-07-08 22:43:32 -0400

    Add additional tests

--- famplex/tests/test_graph.py
+++ famplex/tests/test_graph.py
@@ -18,14 +18,45 @@ def famplex_graph():
                           (('HGNC', 'GENE'), False)])
 def test_in_famplex(famplex_graph, test_input, expected):
         assert famplex_graph.in_famplex(*test_input) == expected
-        
+
 
 @pytest.mark.parametrize('test_input,expected',
-                         [[('FPLX', 'Voltage_gated_ion_channels'), []],
-                          [('FPLX', 'SCN'),
+                         [(('FPLX', 'Voltage_gated_ion_channels'), []),
+                          (('FPLX', 'SCN'),
                            [('FPLX', 'Sodium_channels'),
-                            ('FPLX', 'Voltage_gated_ion_channels')]],
-                  [('FPLX', 'TRP'), [('FPLX', 'Voltage_gated_ion_channels')]],
-                  [('HGNC', 'TRPA1'), [('FPLX', 'TRP')]]])
+                            ('FPLX', 'Voltage_gated_ion_channels')]),
+                  (('FPLX', 'TRP'), [('FPLX', 'Voltage_gated_ion_channels')]),
+                          (('HGNC', 'TRPA1'), [('FPLX', 'TRP')])])
 def test_parent_terms(famplex_graph, test_input, expected):
         assert famplex_graph.parent_terms(*test_input) == expected
+
+
+@pytest.mark.parametrize('test_input,expected',
+                         [(('HGNC', 'TRPA1'), []),
+                          (('FPLX', 'ESR'),
+                           [('HGNC', 'ESR1'), ('HGNC', 'ESR2')])])
+def test_child_terms(famplex_graph, test_input, expected):
+    assert famplex_graph.child_terms(*test_input) == expected
+
+
+@pytest.mark.parametrize('test_input,expected',
+                         [(('HGNC', 'ESR1'), [('FPLX', 'ESR')]),
+                          (('FPLX', 'ESR'), [('FPLX', 'ESR')]),
+                          (('HGNC', 'SCN8A'),
+                           [('FPLX', 'Cation_channels'),
+                            ('FPLX', 'Voltage_gated_ion_channels')])])
+def test_root_terms(famplex_graph, test_input, expected):
+    assert famplex_graph.root_terms(*test_input) == expected
+
+
+@pytest.mark.parametrize('test_input,expected',
+                         [(('FPLX', 'ESR'), []),
+                          (('HGNC', 'ESR1'), [('FPLX', 'ESR')]),
+                          (('HGNC', 'SCN8A'),
+                          [('FPLX',
+                            'Sodium_voltage_gated_channel_alpha_subunits'),
+                           ('FPLX', 'SCN'), ('FPLX', 'Sodium_channels'),
+                           ('FPLX', 'Voltage_gated_ion_channels'),
+                           ('FPLX', 'Cation_channels')])])
+def test_ancestral_terms(famplex_graph, test_input, expected):
+    assert famplex_graph.ancestral_terms(*test_input) == expected
